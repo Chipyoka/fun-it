5e8d79eac78d39ed79b8e78f7da65352ade56caa
Chipyoka <mwapechipyoka18@gmail.com>
2025-05-02 11:02:17 +0200

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+
+    return(
+        <>
+
+        <section>
+            <p>Welcome from home</p>
+        </section>
+        </>
+    )
+}
+
+export default Home;
